b8fbb7be6b0cc1509fb1a75a6b9695bdc08d9a83
len4cf <helenacarvalhoferreira05@gmail.com>
2024-06-22 15:29:21 -0300

    logo link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,9 +4,12 @@ const Header = () => {
   return (
     <header className="bg-zinc-900 shadow-xl">
       <div className="py-3 flex md:flex-row flex-col items-center justify-around text-white">
-        <h1 className="shrikhand text-2xl">
-          Cine<span className="text-green-500 shrikhand">Lena 📽️</span>
-        </h1>
+        
+        <Link to={"/"}>
+          <h1 className="shrikhand text-2xl">
+            Cine<span className="text-green-500 shrikhand">Lena 📽️</span>
+          </h1>
+        </Link>
         <nav className="flex mt-2 gap-4 font-semibold">
           <Link className="hover:bg-zinc-800 p-3 rounded" to={"/"}>
             Inicio
